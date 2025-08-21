b2e9d5d59d40cec37d2b57975b4fc3b61f13b43a
Gosxa <ihorkondrashov@gmail.com>
2025-08-21 20:06:35 +0300

    solution

--- task.sql
+++ task.sql
@@ -1,12 +1,6 @@
 CREATE DATABASE ShopDB; 
 USE ShopDB; 
 
--- Create a table to store countries 
-CREATE TABLE Countries (
-    ID INT,
-    Name VARCHAR(50),
-    PRIMARY KEY (ID)
-) ENGINE=InnoDB;
 
 CREATE TABLE GeoIPCache (
     ID INT PRIMARY KEY AUTO_INCREMENT,
